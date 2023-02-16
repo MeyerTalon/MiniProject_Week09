23fa717f682f6fd109aa6bc4c5a400d5ecdff039
Geez-Us <noahsullivan382@gmail.com>
2023-02-15 21:34:40 -0800

    update

--- java.js
+++ java.js
@@ -44,9 +44,20 @@ inquirer
       },
     ])
     .then((data) => {
+      console.log(data.name);
       const filename = `index.HTML`;
-
-      fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+      fs.writeFile(filename, `<!DOCTYPE html>
+      <html>
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+      </head>
+      <body>
+      <h1>${data.name}</h1>
+      <h3>${data.github}</h3>
+      <div>${data.about}</div>
+      </body>
+      </html>`, (err) =>
         err ? console.log(err) : console.log("Success!")
       );
     });
