fac768b70e840c1be0aa3556d08cd6bb2297ed6a
Jacob Goreham <jacobgoreham@gmail.com>
2023-02-15 21:03:39 -0800

    fixed layout

--- java.js
+++ java.js
@@ -1,74 +1,54 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "checkbox",
-      message: "What languages do you know?",
-      name: "stack",
-      choices: ["HTML", "CSS", "JavaScript", "MySQL"],
-    },
-    {
-      type: "list",
-      message: "What is your preferred method of communication?",
-      name: "contact",
-      choices: ["email", "phone", "telekinesis"],
-    },
-  ])
-  .then((data) => {
-    const filename = `index.HTML`;
-
-    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
-      err ? console.log(err) : console.log("Success!")
-    );
-  });
-
 const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name? (Required)",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter your name!");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your Github Username (Required)",
-      validate: (nameInput) => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter your Github Username!");
-          return false;
-        }
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your Github Username (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your Github Username!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "confirm",
-      name: "confirmAbout",
-      message:
-        'Would you like to enter some information about yourself for an "About" section?',
-      default: true,
-    },
-    {
-      type: "input",
-      name: "about",
-      message: "Provide some information about yourself:",
-      when: ({ confirmAbout }) => confirmAbout,
-    },
-  ]);
+      {
+        type: "confirm",
+        name: "confirmAbout",
+        message:
+          'Would you like to enter some information about yourself for an "About" section?',
+        default: true,
+      },
+      {
+        type: "input",
+        name: "about",
+        message: "Provide some information about yourself:",
+        when: ({ confirmAbout }) => confirmAbout,
+      },
+    ])
+    .then((data) => {
+      const filename = `index.HTML`;
+
+      fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+        err ? console.log(err) : console.log("Success!")
+      );
+    });
 };
